63bbd73d4612e76540d1ed46dc1432a15e285ac2
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-04-25 16:26:12 +0200

    Implement Search Function using OpenAI embeddings

--- movie_recs.py
+++ movie_recs.py
@@ -1,9 +1,32 @@
 import src.db as db
 import src.model as model
+import src.openai
 
-for doc in db.collection.find(
-    {"plot_embedding_hf": {"$exists": False}, "plot": {"$exists": True}}
-):
-    doc["plot_embedding_hf"] = model.generate_embedding_locally(doc["plot"])
-    print(f"{doc['title']} = {doc['plot_embedding_hf'][:3]}")
-    db.collection.replace_one({"_id": doc["_id"]}, doc)
+
+def batch_generate_embeddings():
+    for doc in db.movies.find(
+        {"plot_embedding_hf": {"$exists": False}, "plot": {"$exists": True}}
+        # {"plot": {"$exists": True}}
+    ):
+        doc["plot_embedding_hf"] = model.generate_embedding_locally(doc["plot"])
+        print(f"{doc['title']} = {doc['plot_embedding_hf'][:3]}")
+        db.movies.replace_one({"_id": doc["_id"]}, doc)
+
+
+def search_embeddings():
+    query = "imaginary characters from outer space at war"
+    results = db.embedded_movies.aggregate([{
+        "$vectorSearch": {
+            "queryVector": src.openai.generate_embeddings(query),
+            "path": "plot_embedding",
+            "numCandidates": 100,
+            "limit": 4,
+            "index": "PlotSemanticSearch",
+        }
+    }])
+
+    for document in results:
+        print(f"Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n")
+
+
+search_embeddings()
